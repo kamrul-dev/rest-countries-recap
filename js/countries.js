b674f2e2be8d3d76db813cec6b88f63cdd962f3f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-14 21:18:32 +0600

    UI setup for displaying the data

--- js/countries.js
+++ js/countries.js
@@ -6,14 +6,18 @@ const loadCountries = () =>{
 }
 // display countries
 const displayCountries = countries =>{
-    console.log(countries);
+    console.log(countries[0]);
     const countriesHTML = countries.map(country => getCountryHTML(country))
-    console.log(countriesHTML[0]);
+    // console.log(countriesHTML[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML.join(' ');
+
 }
 const getCountryHTML = country => {
     return `
-        <div>
+        <div class="country">
             <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
         </div>
     `
 }
