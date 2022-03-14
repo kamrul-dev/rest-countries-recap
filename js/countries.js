301d98e0080427e6b43ef5bb3302acf84526048c
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-14 21:10:38 +0600

    create a html element

--- js/countries.js
+++ js/countries.js
@@ -7,5 +7,15 @@ const loadCountries = () =>{
 // display countries
 const displayCountries = countries =>{
     console.log(countries);
+    const countriesHTML = countries.map(country => getCountryHTML(country))
+    console.log(countriesHTML[0]);
 }
+const getCountryHTML = country => {
+    return `
+        <div>
+            <h2>${country.name.common}</h2>
+        </div>
+    `
+}
+
 loadCountries();
